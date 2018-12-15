c30978f1cca42593518e9ea9a2993d83ffaf414c
Lynalmost <33012209+Lynalmost@users.noreply.github.com>
2018-12-15 15:49:26 +0800

    Create StudentTableViewCell.swift

---
+++ homework_8/StudentTableViewCell.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+class StudentTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var age: UILabel!
+    @IBOutlet weak var gender: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
