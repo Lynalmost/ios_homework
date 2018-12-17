d84a25308861e54c66afb9f4a17a0c4166a886b3
Lynalmost <33012209+Lynalmost@users.noreply.github.com>
2018-12-17 15:41:06 +0800

    Create SecondViewController.swift

---
+++ homework_11/SecondViewController.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    var name = ""
+    var number = ""
+    var delegate: StudentProtocol?
+    @IBOutlet var tfnumber: UITextField!
+    @IBOutlet var tfname: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tfname.text = name
+        tfnumber.text = number
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func back(_ sender: Any) {
+        number = tfnumber.text!
+        name = tfname.text!
+        
+        delegate?.change(name:name,number:number)
+        
+        navigationController?.popViewController(animated: true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
