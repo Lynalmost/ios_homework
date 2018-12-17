a41b993218e3444d43a78821939c29f9ddc2a760
Lynalmost <33012209+Lynalmost@users.noreply.github.com>
2018-12-17 16:13:48 +0800

    Create ViewController.swift

---
+++ homework_10/task2/ViewController.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var outerView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let myView1 = UIView(frame: CGRect(x: 30, y: 30, width: 200, height: 200))
+        myView1.backgroundColor = UIColor.green
+        let myView2 = UIView(frame: CGRect(x: 30, y: 30, width: 200, height: 200))
+        myView2.backgroundColor = UIColor.red
+        outerView.insertSubview(myView1, at: 0)
+        outerView.insertSubview(myView2, at: 1)
+    }
+
+    @IBAction func btnClicked(_ sender: Any) {
+        UIView.transition(with: outerView, duration: 2, options: .transitionCurlUp, animations: {
+            self.outerView.exchangeSubview(at: 0, withSubviewAt: 1)
+        }, completion: nil)
+    }
+    
+}
